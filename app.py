e2978744fe7a89a8b53b41b88326928d2d246b26
vboxuser <ameerarayan2@gmail.com>
2025-05-22 17:08:20 +0000

    modify app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import subprocess
-
+import time
 app = FastAPI()
 
 # Enable CORS so the UI EC2 can call this server
@@ -16,7 +16,7 @@ app.add_middleware(
 async def generate_outfit(request: Request):
     data = await request.json()
     prompt = data.get("prompt", "")
-
+    start_time=time.time()
     full_prompt = (f"Give me exactly 3 outfit ideas for a girl for: {prompt}. "
                    f" Each outfit must be clearly numbered and described briefly. Format: "
                    f"1. ... , 2. ... , 3. ..."
@@ -25,9 +25,13 @@ async def generate_outfit(request: Request):
 
     try:
         output = subprocess.check_output([
-            "/usr/local/bin/ollama", "run", "gemma:2b", full_prompt
+            "/usr/local/bin/ollama", "run", "tinyllama", full_prompt
         ]).decode("utf-8")
+        end_time= time.time()
+        duration=round(end_time-start_time,2)
+        print(duration)
     except Exception as e:
         return {"error": str(e)}
 
-    return {"response": output}
+    return {"response": output ,
+            "duration_seconds": duration}
